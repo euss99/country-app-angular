d68236b3a89eb2ac69af61608c27898d0276e5a1
Uriel Solis Salinas <euss99@hotmail.com>
2025-07-02 14:47:32 -0600

    Enhance by-country and by-region pages with search functionality
    - Updated HTML templates for by-country and by-region pages to include CountrySearch and CountryList components.
    - Implemented search event handling in ByCountryPage and ByRegionPage components to log search terms.
    - Improved layout structure for better user experience.

--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 
+import { CountrySearch } from '@app/country/components/country-search/country-search.component';
+import { CountryList } from '@app/country/components/country-list/country-list.component';
+
 @Component({
   selector: 'by-country-page',
-  imports: [],
+  imports: [CountrySearch, CountryList],
   templateUrl: './by-country-page.component.html',
 })
-export default class ByCountryPage {}
+export default class ByCountryPage {
+  onSearchByCountry(term: string) {
+    console.log('Desde ByCountryPage', term);
+  }
+}
--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 
+import { CountrySearch } from '@app/country/components/country-search/country-search.component';
+import { CountryList } from '@app/country/components/country-list/country-list.component';
+
 @Component({
   selector: 'by-region-page',
-  imports: [],
+  imports: [CountrySearch, CountryList],
   templateUrl: './by-region-page.component.html',
 })
-export default class ByRegionPage {}
+export default class ByRegionPage {
+  onSearchByRegion(term: string) {
+    console.log('Desde ByRegionPage', term);
+  }
+}
