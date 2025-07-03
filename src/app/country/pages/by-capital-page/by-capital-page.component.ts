b468bf7b33d121a9fd273e82a1e8975c9095e947
Uriel Solis Salinas <euss99@hotmail.com>
2025-07-03 12:55:35 -0600

    Refactor country page components to utilize rxResource for improved data handling
    - Updated ByCapitalPage, ByCountryPage, and ByRegionPage to replace resource with rxResource for better integration with RxJS.
    - Simplified data fetching logic by using stream instead of loader, enhancing readability and performance.
    - Ensured consistent handling of empty query cases across all pages.

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject, signal, resource, computed } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 
-import { firstValueFrom } from 'rxjs';
+import { of } from 'rxjs';
 
 import { CountryList } from '@app/country/components/country-list/country-list.component';
 import { CountrySearch } from '@app/country/components/country-search/country-search.component';
@@ -14,14 +15,12 @@ export default class ByCapitalPage {
   private countryService = inject(CountryService);
   public query = signal<string>('');
 
-  public countryResource = resource({
+  public countryResource = rxResource({
     params: () => ({ query: this.query() }),
-    loader: async ({ params }) => {
-      if (!params.query) return [];
+    stream: ({ params }) => {
+      if (!params.query) return of([]);
 
-      return await firstValueFrom(
-        this.countryService.searchByCapital(params.query),
-      );
+      return this.countryService.searchByCapital(params.query);
     },
   });
 
